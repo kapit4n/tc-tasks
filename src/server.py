0ebd11acec28ebeb39a264d574826d41ffe7a0db
Luis Arce <luis.arce22@gmail.com>
2022-03-18 19:31:33 -0400

    Init resources

--- src/server.py
+++ src/server.py
@@ -1,15 +1,26 @@
-from flask import Flask
-
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'helo': 'workd'}
+tasks = [{"id": "1", "name": 'Generics'}, {"id": "2", "name": 'Routines'}]
 
-api.add_resource(HelloWorld, '/')
+class TaskListResource(Resource):
+    def get(self):
+        return tasks
+
+class TaskResource(Resource):
+    def get(self, task_id):
+        return tasks[int(task_id)]
+
+    def post(self):
+        post_body = request.get_json()
+        tasks.append({"name": post_body['name'], "id": str(len(tasks))})
+        return tasks[-1]
+
+api.add_resource(TaskListResource, '/tasks')
+api.add_resource(TaskResource, '/tasks/<task_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
