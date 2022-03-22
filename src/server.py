4a673fa376f138d8b3fc64d32e9606c5099d7571
Luis Arce <luis.arce22@gmail.com>
2022-03-21 20:49:26 -0400

    Adding cors

--- src/server.py
+++ src/server.py
@@ -2,10 +2,11 @@ from flask import Flask, request
 from flask_restful import Api
 from resources.task_list_resource import TaskListResource
 from resources.task_resource import TaskResource
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "*"}})
 api = Api(app)
-
 
 api.add_resource(TaskListResource, '/tasks')
 api.add_resource(TaskResource, '/tasks/<task_id>')
