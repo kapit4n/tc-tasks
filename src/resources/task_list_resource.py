f29564e509187be42eee01ff96ce77b15032f0cf
Luis Arce <luis.arce22@gmail.com>
2022-03-21 20:43:41 -0400

    Move post method to task list

--- src/resources/task_list_resource.py
+++ src/resources/task_list_resource.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_restful import Resource
 from constants.tasks import tasks
 
@@ -5,3 +6,9 @@ from constants.tasks import tasks
 class TaskListResource(Resource):
     def get(self):
         return tasks
+
+    def post(self):
+        post_body = request.get_json()
+        tasks.append({"name": post_body['name'], "id": str(len(tasks))})
+        return tasks[-1]
+
--- src/resources/task_resource.py
+++ src/resources/task_resource.py
@@ -1,13 +1,7 @@
 
-from flask import request
 from flask_restful import Resource
 from constants.tasks import tasks
 
 class TaskResource(Resource):
     def get(self, task_id):
         return tasks[int(task_id)]
-
-    def post(self):
-        post_body = request.get_json()
-        tasks.append({"name": post_body['name'], "id": str(len(tasks))})
-        return tasks[-1]
